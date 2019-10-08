bba59dc1a5f67a1e271e5e5bfcf15fb818ce76ae
simb4 <alimzhan.amanov@gmail.com>
2019-10-08 11:15:58 +0600

    feat: heap and heapsort

--- week05/references.cpp
+++ week05/references.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void tipa_modify(int x) {
 	/* here x is local variable */
 	x = 10;
+	cout << "in tipa_modify " << x << "\n"; // 10
 }
 
 void reference_modify(int &x) {
@@ -17,9 +18,9 @@ int main() {
 	int x = 5;
 	cout << "Before " << x << "\n"; // 5
 	tipa_modify(x);
-	cout << "After without reference " << x << "\n";
+	cout << "After without reference " << x << "\n"; // 5
 	reference_modify(x);
-	cout << "After reference modify " << x << "\n";
+	cout << "After reference modify " << x << "\n"; // 10
 
 
 	return 0;
---
+++ week07/heap.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+using namespace std;
+
+int n, k;
+int a[100005];
+
+int heapify_up(int v) {
+	while (v > 1 && a[v] > a[v / 2]) {
+		swap(a[v], a[v/2]);
+		v /= 2;
+	}
+	return v;
+}
+
+void heapify_down(int v) {
+	while (v <= n) {
+		int l = 2 * v; // left son
+		int r = 2 * v + 1; // right son
+		int s = v; // max among a[v],a[l],a[r]
+
+		/* let's find max */
+		if (l <= n && a[l] > a[s])
+			l = s;
+		if (r <= n && a[r] > a[s])
+			r = s;
+
+		// if v is max, then everything is ok
+		if (s == v)
+			return;
+		// go down to vertex s
+		swap(a[v], a[s]);
+		v = s;
+	}
+}
+
+void add(int x) {
+	// add to the end
+	n++;
+	a[n] = x;
+	// propogate (heapify) upwards
+	heapify_up(n);
+}
+
+int top() {
+	// max element is on top
+	return a[1];
+}
+
+int remove_top() { // a.k.a. pop()
+	// swap max with last element
+	swap(a[1], a[n]);
+	// delete last element (delete max)
+	n--;
+	// propogate top element downwards
+	heapify_down(1);
+}
+
+int main() {
+
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", a + i);
+	}
+	int q;
+	scanf("%d", &q);
+	for (int i = 1; i <= q; i++) {
+		int pos, x;
+		scanf("%d %d", &pos, &x);
+		a[pos] += x;
+		int newPos = heapify_up(pos);
+		printf("%d\n", newPos);
+	}
+	for (int i = 1; i <= n; i++) {
+		printf("%d%c", a[i], " \n"[i == n]);
+	}
+
+
+
+	return 0;
+}
